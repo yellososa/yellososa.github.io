446eaf4b1b24e3e6ab37505a8b306bd7a34c6596
Sosa <autumn.ncnh@gmail.com>
2025-02-20 08:28:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function toggleMode() {
     // substituir a imagem
     if (html.classList.contains("light")) {
       // se tiver light mode, adicionar a imagem light
-      img.setAttribute("src", "./assets/mel.jpeg")
+      img.setAttribute("src", "./mel.jpeg")
     } else {
       // set tiver sem light mode, manter a imagem normal
-      img.setAttribute("src", "./assets/mel.jpeg")
+      img.setAttribute("src", "./mel.jpeg")
     }
   }
   
